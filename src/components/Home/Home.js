3b9a2f97956bb264568f704d0d862c3ae550af2c
martuwilson <williner@ibm.com>
2024-08-02 00:06:09 -0300

    change resume

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> MARTIN E.WILLINER</strong>
+                <strong className="main-name"> MARTIN E. WILLINER</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
